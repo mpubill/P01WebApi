4f45825cf95fb410eceda5a4bd73019394decb84
melan <melan@LAPTOP-30ROFJGU>
2023-03-03 10:37:05 -0600

    Agregar archivos de proyecto.

---
+++ P01WebApi/Controllers/equiposController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using P01WebApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace P01WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class equiposController : ControllerBase
+    {
+        private readonly equiposContext _equiposContext;
+
+        public equiposController(equiposContext equiposContexto)
+        {
+            _equiposContext = equiposContexto;
+        }
+
+        [HttpGet]
+        [Route("getAll")]
+        public IActionResult ObtenerEquipos()
+        {
+            List<equipos> ListadoEquipos = (from e in _equiposContext.equipos
+                                            select e).ToList();
+
+
+
+            if (ListadoEquipos.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(ListadoEquipos);
+        }
+    }
+}
---
+++ P01WebApi/Models/equiposContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+namespace P01WebApi.Models
+{
+    public class equiposContext : DbContext
+    {
+
+        public equiposContext(DbContextOptions<equiposContext> option) : base(option) { 
+        
+        }      
+
+        public DbSet<equipos> equipos { get; set; }
+    }
+}
